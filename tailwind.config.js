e66704033a6a66eb2523f3ba2ba6facabf082bbf
Tim Clay <tsclay9@gmail.com>
2020-07-15 17:45:19 -0400

    added min height utilities

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    minHeight: {
+      '0': '0',
+      '1/4': '25%',
+      '1/2': '50%',
+      '3/4': '75%',
+      full: '100%'
+    },
+    extend: {}
   },
   variants: {},
-  plugins: [],
+  plugins: []
 }
