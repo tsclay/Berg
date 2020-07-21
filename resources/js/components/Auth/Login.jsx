186fbdd174b4ff93d1ce83b6f755c88673412850
Tim <tsclay9@gmail.com>
2020-07-21 14:38:15 -0400

    removed unneeded deps

--- resources/js/components/Auth/Login.jsx
+++ resources/js/components/Auth/Login.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 // import axios from 'axios'
 
--- resources/js/components/Auth/Register.jsx
+++ resources/js/components/Auth/Register.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable prettier/prettier */
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 // import axios from 'axios'
 
