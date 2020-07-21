3f8cde35bf61211df5421a57c74bb5a74b4ffc3c
Tim <tsclay9@gmail.com>
2020-07-21 18:24:23 -0400

    cleaned up nav bar ; welcomes user

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -1,11 +1,14 @@
-<nav class="h-30 flex flex-row justify-between bg-gray-500 items-center mb-5 py-2">
+<nav class="h-30 flex flex-row justify-between bg-gray-500 items-center mb-5 py-2 px-2">
   <img id="logo" src="/assets/logo-berg.png" alt="Berg">
+
+  @if (auth()->check())
+  <h4 class="font-bold text-xl text-center">Welcome, {{ auth()->user()->first_name}}</h4>
+  @endif
+
   <ul class="flex flex-row justify-even">
-    @if (auth()->check())
-    <li class="ml-4">{{ auth()->user()->first_name}}</li>
-    @endif
-    <li class="ml-4"><a href="/home">Calculator</a></li>
-    <li class="ml-4"><a href="/account">Account</a></li>
-    <li class="ml-4"><a href="/logout">Logout</a></li>
+
+    <li class="ml-10"><a href="/home">Calculator</a></li>
+    <li class="ml-10"><a href="/account">Account</a></li>
+    <li class="ml-10"><a href="/logout">Logout</a></li>
   </ul>
 </nav>
