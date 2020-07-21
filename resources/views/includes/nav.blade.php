d6d7c49a2b222284df19a3f5206cf5702d1c5a60
Tim <tsclay9@gmail.com>
2020-07-21 14:38:32 -0400

    added link for calculator app

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -4,6 +4,7 @@
     @if (auth()->check())
     <li class="ml-4">{{ auth()->user()->first_name}}</li>
     @endif
+    <li class="ml-4"><a href="/home">Calculator</a></li>
     <li class="ml-4"><a href="/account">Account</a></li>
     <li class="ml-4"><a href="/logout">Logout</a></li>
   </ul>
