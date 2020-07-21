daf2ebbc6b398cc8910716c164ff2f53201990f7
Tim <tsclay9@gmail.com>
2020-07-21 18:11:27 -0400

    added favicon and re-sized css

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -1,5 +1,5 @@
-<nav class="h-30 flex flex-row justify-between bg-gray-500 items-center mb-5">
-  <h1 class="text-5xl">Berg</h1>
+<nav class="h-30 flex flex-row justify-between bg-gray-500 items-center mb-5 py-2">
+  <img id="logo" src="/assets/logo-berg.png" alt="Berg">
   <ul class="flex flex-row justify-even">
     @if (auth()->check())
     <li class="ml-4">{{ auth()->user()->first_name}}</li>
